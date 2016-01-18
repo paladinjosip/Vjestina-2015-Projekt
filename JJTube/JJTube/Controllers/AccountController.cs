6eef9acfdd53d65ca54213259ef3bbf52c8f1f62
tamaramilisa <tamara.milisa@fer.hr>
2016-01-18 13:06:26 +0100

    tamarin register

---
+++ JJTube/JJTube/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using JJTube.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JJTube.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel loginUser)
+        {
+            if (ModelState.IsValid) { 
+                //Odradi povezivanje sa bazom, ako user postoji => cookie + redirect na myLists
+
+            }
+            return View();
+        }
+
+	}
+}
