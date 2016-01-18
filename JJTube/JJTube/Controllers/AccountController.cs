baa6a0b044b26cb6bc2694c5fc516ed7420dd52e
Josip Paladin <josip.paladin@fer.hr>
2016-01-18 15:48:49 +0100

    Login pri kraju

--- JJTube/JJTube/Controllers/AccountController.cs
+++ JJTube/JJTube/Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +11,7 @@ namespace JJTube.Controllers
 {
     public class AccountController : Controller
     {
+        JJTubeDbContext context = new JJTubeDbContext();
         //
         // GET: /Account/
         public ActionResult Login()
@@ -19,9 +22,19 @@ namespace JJTube.Controllers
         [HttpPost]
         public ActionResult Login(LoginViewModel loginUser)
         {
+         
             if (ModelState.IsValid) { 
                 //Odradi povezivanje sa bazom, ako user postoji => cookie + redirect na myLists
 
+                var sha1 = new SHA1CryptoServiceProvider();
+                var password = sha1.ComputeHash(Encoding.Unicode.GetBytes(loginUser.Password));
+
+                People user = context.People.FirstOrDefault(x => (x.Username == loginUser.Username && x.Password.Equals(password)));
+                if (user != null) { 
+                    //Uspjesan login
+                    return RedirectToAction("Index", "Private");
+
+                }
             }
             return View();
         }
